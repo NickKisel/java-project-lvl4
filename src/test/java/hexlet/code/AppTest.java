9d17fc371be7ea5d44d396545cc4b46e73d2a30a
Nick Kisel <insherlolqq@gmail.com>
2022-03-07 13:26:24 +0300

    add more test

--- src/test/java/hexlet/code/AppTest.java
+++ src/test/java/hexlet/code/AppTest.java
@@ -1,5 +1,7 @@
 package hexlet.code;
 
+import hexlet.code.domain.query.QUrl;
+import hexlet.code.domain.Url;
 import io.ebean.DB;
 import io.ebean.Transaction;
 import io.javalin.Javalin;
@@ -50,6 +52,68 @@ class AppTest {
         String content = response.getBody();
         assertThat(content).contains("Анализатор страниц");
         assertThat(content).contains("Бесплатно проверяйте сайты на SEO пригодность");
-        assertThat(content).contains("Nickolai Kisel");
+        assertThat(content).contains("Nick Kisel");
+    }
+
+    @Test
+    void testCreateValidUrl() {
+        String name = "https://ru.wikipedia.org";
+        HttpResponse<String> response = Unirest
+                .post(baseUrl + "/urls")
+                .field("url", name)
+                        .asEmpty();
+
+        assertThat(response.getStatus()).isEqualTo(302);
+        assertThat(response.getHeaders().getFirst("Location")).isEqualTo("/urls");
+
+        Url actualUrl = new QUrl()
+                .name.equalTo(name)
+                .findOne();
+        assertThat(actualUrl).isNotNull();
+        assertThat(actualUrl.getName()).isEqualTo(name);
+    }
+
+    @Test
+    void testCreateInvalidUrl() {
+        String inputUrl = "Qwerty123";
+        HttpResponse<String> responsePost = Unirest
+                .post(baseUrl + "/urls")
+                .field("url", inputUrl)
+                .asEmpty();
+
+        assertThat(responsePost.getStatus()).isEqualTo(302);
+        assertThat(responsePost.getHeaders().getFirst("Location")).isEqualTo("/");
+
+        HttpResponse<String> response = Unirest
+                .get(baseUrl + "/urls")
+                .asString();
+        String body = response.getBody();
+
+        assertThat(response.getStatus()).isEqualTo(200);
+        assertThat(body).doesNotContain(inputUrl);
+        assertThat(body).contains("Некорректный URL");
+
+        Url actualUrl = new QUrl()
+                .name.equalTo(inputUrl)
+                .findOne();
+
+        assertThat(actualUrl).isNull();
+    }
+
+    @Test
+    void testShowUrls() {
+        HttpResponse<String> postResponse1 = Unirest
+                .post(baseUrl + "/urls")
+                .field("url", "https://ru.wikipedia.org")
+                .asEmpty();
+        HttpResponse<String> postResponse2 = Unirest
+                .post(baseUrl + "/urls")
+                .field("url", "https://ru.hexlet.io")
+                .asEmpty();
+        HttpResponse<String> response = Unirest.get(baseUrl + "/urls").asString();
+        assertThat(response.getStatus()).isEqualTo(200);
+        String content = response.getBody();
+        assertThat(content).contains("https://ru.wikipedia.org");
+        assertThat(content).contains("https://ru.hexlet.io");
     }
 }
