2c049a0106d70a89fe72e9df6fade91472d3b602
Nick Kisel <insherlolqq@gmail.com>
2022-03-07 13:28:34 +0300

    rework logic for create user

---
+++ src/main/java/hexlet/code/controllers/UrlController.java
@@ -0,0 +1,83 @@
+package hexlet.code.controllers;
+
+import hexlet.code.domain.Url;
+import hexlet.code.domain.query.QUrl;
+import io.ebean.PagedList;
+import io.javalin.http.Handler;
+import io.javalin.http.NotFoundResponse;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public final class UrlController {
+    public static Handler listUrls = ctx -> {
+        int page = ctx.queryParamAsClass("page", Integer.class).getOrDefault(1) - 1;
+        int rowsPerPage = 10;
+
+        PagedList<Url> pagedUrls = new QUrl()
+                .setFirstRow(page * rowsPerPage)
+                .setMaxRows(rowsPerPage)
+                .orderBy()
+                .id.asc()
+                .findPagedList();
+
+        List<Url> urls = pagedUrls.getList();
+
+        int lastPage = pagedUrls.getTotalPageCount() + 1;
+        int currentPage = pagedUrls.getPageIndex() + 1;
+        List<Integer> pages = IntStream
+                .range(1, lastPage)
+                .boxed()
+                .collect(Collectors.toList());
+
+        ctx.attribute("urls", urls);
+        ctx.attribute("currentPage", currentPage);
+        ctx.attribute("pages", pages);
+        ctx.render("urls/index.html");
+    };
+
+    public static Handler createUrl = ctx -> {
+        try {
+
+            URL url = new URL(ctx.formParam("url"));
+            String normalizedUrl = url.getProtocol() + "://" + url.getAuthority();
+
+            boolean urlExist = new QUrl()
+                    .name.equalTo(normalizedUrl)
+                    .exists();
+
+            if (urlExist) {
+                ctx.sessionAttribute("flash", "Страница уже существует");
+                ctx.sessionAttribute("flash-type", "info");
+            } else {
+                new Url(normalizedUrl).save();
+
+                ctx.sessionAttribute("flash", "Страница успешно добавлена");
+                ctx.sessionAttribute("flash-type", "success");
+            }
+        } catch (MalformedURLException e) {
+            ctx.sessionAttribute("flash", "Некорректный URL");
+            ctx.sessionAttribute("flash-type", "danger");
+            ctx.redirect("/");
+            return;
+        }
+        ctx.redirect("/urls");
+    };
+
+    public static Handler showUrl = ctx -> {
+        long id = ctx.pathParamAsClass("id", Integer.class).getOrDefault(null);
+        Url url = new QUrl()
+                .id.equalTo(id)
+                .findOne();
+
+        if (url == null) {
+            throw new NotFoundResponse();
+        }
+
+        ctx.attribute("url", url);
+        ctx.render("urls/show.html");
+    };
+}
