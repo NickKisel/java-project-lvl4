d672a0c2ebecbd3ed34f2bf791a4dd4d7a20bac2
Nick Kisel <insherlolqq@gmail.com>
2022-03-07 17:29:17 +0300

    fix connection between Url and UrlCheck

--- src/main/java/hexlet/code/controllers/UrlController.java
+++ src/main/java/hexlet/code/controllers/UrlController.java
@@ -1,14 +1,24 @@
 package hexlet.code.controllers;
 
 import hexlet.code.domain.Url;
+import hexlet.code.domain.UrlCheck;
 import hexlet.code.domain.query.QUrl;
+import hexlet.code.domain.query.QUrlCheck;
 import io.ebean.PagedList;
 import io.javalin.http.Handler;
 import io.javalin.http.NotFoundResponse;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+import kong.unirest.UnirestException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -17,11 +27,16 @@ public final class UrlController {
         int page = ctx.queryParamAsClass("page", Integer.class).getOrDefault(1) - 1;
         int rowsPerPage = 10;
 
+        QUrl url = QUrl.alias();
+        QUrlCheck check = QUrlCheck.alias();
+
         PagedList<Url> pagedUrls = new QUrl()
                 .setFirstRow(page * rowsPerPage)
                 .setMaxRows(rowsPerPage)
-                .orderBy()
-                .id.asc()
+                .select(url.id, url.name)
+                .orderBy().id.asc()
+                .urlChecks.fetch(check.createdAt, check.statusCode)
+                .orderBy().urlChecks.id.desc()
                 .findPagedList();
 
         List<Url> urls = pagedUrls.getList();
@@ -41,7 +56,6 @@ public final class UrlController {
 
     public static Handler createUrl = ctx -> {
         try {
-
             URL url = new URL(ctx.formParam("url"));
             String normalizedUrl = url.getProtocol() + "://" + url.getAuthority();
 
@@ -73,11 +87,46 @@ public final class UrlController {
                 .id.equalTo(id)
                 .findOne();
 
+        List<UrlCheck> urlChecks = new QUrlCheck()
+                .url.equalTo(url)
+                .orderBy().id.desc()
+                .findList();
+
         if (url == null) {
             throw new NotFoundResponse();
         }
 
+        ctx.attribute("urlChecks", urlChecks);
         ctx.attribute("url", url);
         ctx.render("urls/show.html");
+    };
+
+    public static Handler checkUrl = ctx -> {
+        long id = ctx.pathParamAsClass("id", Integer.class).getOrDefault(null);
+        Url url = new QUrl()
+                .id.equalTo(id)
+                .findOne();
+        String name = url.getName();
+        try {
+            HttpResponse<String> responseGet = Unirest.get(name).asString();
+
+            int statusCode = responseGet.getStatus();
+
+            Document document = Jsoup.parse(responseGet.getBody());
+
+            String title = document.title();
+            String description = Objects.requireNonNull(document.selectFirst("meta[name=description]")).attr("content");
+            String h1 = Objects.requireNonNull(document.selectFirst("h1")).text();
+
+            UrlCheck urlCheck = new UrlCheck(statusCode, title, h1, description, url);
+            urlCheck.save();
+
+            ctx.sessionAttribute("flash", "Сайт успешно проверен");
+            ctx.sessionAttribute("flash-type", "success");
+        } catch (UnirestException exception) {
+            ctx.sessionAttribute("flash", "Не удалось проверить сайт");
+            ctx.sessionAttribute("flash-type", "danger");
+        }
+        ctx.redirect("/urls/" + id);
     };
 }
