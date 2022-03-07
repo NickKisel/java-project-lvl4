288d18a877201f5772bf9607712a07cffa3f2a3a
Nick Kisel <insherlolqq@gmail.com>
2022-03-07 13:28:09 +0300

    rework Url model

--- src/main/java/hexlet/code/domain/Url.java
+++ src/main/java/hexlet/code/domain/Url.java
@@ -1,36 +1,28 @@
 package hexlet.code.domain;
 
+import io.ebean.Model;
 import io.ebean.annotation.WhenCreated;
-import io.ebean.annotation.WhenModified;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Lob;
 import java.time.Instant;
 
 @Entity
-public final class Url {
+public final class Url extends Model {
 
     @Id
     private long id;
 
     private String name;
 
-    @Lob
-    private String description;
-
     @WhenCreated
     private Instant createdAt;
-
-    @WhenModified
-    private  Instant updatedAt;
 
     public Url() {
     }
 
-    public Url(String name, String description) {
+    public Url(String name) {
         this.name = name;
-        this.description = description;
     }
 
     public long getId() {
@@ -41,15 +33,7 @@ public final class Url {
         return name;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
     public Instant getCreatedAt() {
         return createdAt;
-    }
-
-    public Instant getUpdatedAt() {
-        return updatedAt;
     }
 }
